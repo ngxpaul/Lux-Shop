9d2e0c38e8c236c25c1074c46128349f7e41b316
ngxpaul <phongnx.22git@vku.udn.vn>
2024-02-12 16:39:42 +0700

    use Context CartContext

--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -1,5 +1,6 @@
 import { createContext } from "react";
 export const CartContext = createContext({
     item: [],
-    addItemToCart: (item) => {},//this is may be not use but it helps auto completion
+    addItemToCart: () => {},//this is may be not use but it helps auto completion
+    updateItemQuantity: () => {}
 });
